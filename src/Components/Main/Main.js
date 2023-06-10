934c46f031b0560f3533510ecc283973082040c8
Behruzbek <behruzbekmuxtorov06@gmail.com>
2023-06-10 15:50:24 +0500

    i added blyud page

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -13,22 +13,24 @@ function Main() {
         <h1>
           Наши <span>Блюда</span>
         </h1>
-        <div className="price">
+        <div className="blyud-price">
           <div className="img">
             <img src={imgpizza} alt="" />
-            <div className="xamburger">
+            <div className="blyud-xamburger">
               <img src={imghamburger} alt="" />
               <img src={imghamburger} alt="" />
               <img src={imghamburger} alt="" />
             </div>
           </div>
-          <div className="text">
-            <p>Гамбургер мини ---------- 220 ₽</p>
-            <p>Гамбургер мини ---------- 220 ₽</p>
-            <p>Гамбургер мини ---------- 220 ₽</p>
+          <div className="blyud-text">
+            <p>Гамбургер мини   ------------------------   220 ₽</p>
+            <p>Гамбургер мини   ------------------------   220 ₽</p>
+            <p>Гамбургер мини   ------------------------   220 ₽</p>
           </div>
         </div>
       </div>
+
+      
     </div>
   );
 }
