a0b6995d29a6d4c04cad863a77d4d0d9f1db0e27
Behruzbek <behruzbekmuxtorov06@gmail.com>
2023-06-11 07:05:36 +0500

    Menu page has been Done

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Main.css"
 import imgpizza from "../IMG/pizza.png"
 import imghamburger from "../IMG/hamburger.png";
-
+import maskgr from "../IMG/Mask Group.png"
 function Main() {
   return (
     <div className="main">
@@ -23,14 +23,67 @@ function Main() {
             </div>
           </div>
           <div className="blyud-text">
-            <p>Гамбургер мини   ------------------------   220 ₽</p>
-            <p>Гамбургер мини   ------------------------   220 ₽</p>
-            <p>Гамбургер мини   ------------------------   220 ₽</p>
+            <p>Гамбургер мини ------------------------ 220 ₽</p>
+            <p>Гамбургер мини ------------------------ 220 ₽</p>
+            <p>Гамбургер мини ------------------------ 220 ₽</p>
           </div>
         </div>
       </div>
 
+      <div className="main-menu">
+        <div className="menu">
+          <h1>Наше меню</h1>
+          <div className="burger-blok">
+              <div className="burger-box">
+                <div className="img">
+                  <img src={maskgr} alt="" />
+                <div className="price">
+                  <p>420</p>
+                </div>
+                  
+                </div>
+                <div className="text">
+                  <h3>Гамбургер макси</h3>
+                  <p>Максимально толстый 
+слой мяса</p>
+                  <button>ЗАКАЗАТЬ</button>
+                </div>
+              </div>
 
+              <div className="burger-box">
+                <div className="img">
+                  <img src={maskgr} alt="" />
+                <div className="price">
+                  <p>420</p>
+                </div>
+                  
+                </div>
+                <div className="text">
+                  <h3>Гамбургер макси</h3>
+                  <p>Максимально толстый 
+слой мяса</p>
+                  <button>ЗАКАЗАТЬ</button>
+                </div>
+              </div>
+
+              <div className="burger-box">
+                <div className="img">
+                  <img src={maskgr} alt="" />
+                <div className="price">
+                  <p>420</p>
+                </div>
+                  
+                </div>
+                <div className="text">
+                  <h3>Гамбургер макси</h3>
+                  <p>Максимально толстый 
+слой мяса</p>
+                  <button>ЗАКАЗАТЬ</button>
+                </div>
+              </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
