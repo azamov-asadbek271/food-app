7fdf2841d05cebf9a631a9ac77fb9b47b36e0268
Behruzbek <behruzbekmuxtorov06@gmail.com>
2023-06-11 08:01:41 +0500

    comment has been done

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,8 +1,10 @@
 import React from "react";
-import "./Main.css"
-import imgpizza from "../IMG/pizza.png"
+import "./Main.css";
+import imgpizza from "../IMG/pizza.png";
 import imghamburger from "../IMG/hamburger.png";
-import maskgr from "../IMG/Mask Group.png"
+import maskgr from "../IMG/Mask Group.png";
+import man from "../IMG/man.png";
+
 function Main() {
   return (
     <div className="main">
@@ -34,53 +36,67 @@ function Main() {
         <div className="menu">
           <h1>Наше меню</h1>
           <div className="burger-blok">
-              <div className="burger-box">
-                <div className="img">
-                  <img src={maskgr} alt="" />
+            <div className="burger-box">
+              <div className="img">
+                <img src={maskgr} alt="" />
                 <div className="price">
                   <p>420</p>
                 </div>
-                  
-                </div>
-                <div className="text">
-                  <h3>Гамбургер макси</h3>
-                  <p>Максимально толстый 
-слой мяса</p>
-                  <button>ЗАКАЗАТЬ</button>
-                </div>
               </div>
+              <div className="text">
+                <h3>Гамбургер макси</h3>
+                <p>Максимально толстый слой мяса</p>
+                <button>ЗАКАЗАТЬ</button>
+              </div>
+            </div>
 
-              <div className="burger-box">
-                <div className="img">
-                  <img src={maskgr} alt="" />
+            <div className="burger-box">
+              <div className="img">
+                <img src={maskgr} alt="" />
                 <div className="price">
                   <p>420</p>
                 </div>
-                  
-                </div>
-                <div className="text">
-                  <h3>Гамбургер макси</h3>
-                  <p>Максимально толстый 
-слой мяса</p>
-                  <button>ЗАКАЗАТЬ</button>
-                </div>
               </div>
+              <div className="text">
+                <h3>Гамбургер макси</h3>
+                <p>Максимально толстый слой мяса</p>
+                <button>ЗАКАЗАТЬ</button>
+              </div>
+            </div>
 
-              <div className="burger-box">
-                <div className="img">
-                  <img src={maskgr} alt="" />
+            <div className="burger-box">
+              <div className="img">
+                <img src={maskgr} alt="" />
                 <div className="price">
                   <p>420</p>
                 </div>
-                  
-                </div>
-                <div className="text">
-                  <h3>Гамбургер макси</h3>
-                  <p>Максимально толстый 
-слой мяса</p>
-                  <button>ЗАКАЗАТЬ</button>
-                </div>
               </div>
+              <div className="text">
+                <h3>Гамбургер макси</h3>
+                <p>Максимально толстый слой мяса</p>
+                <button>ЗАКАЗАТЬ</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* comments */}
+      <div className="main-comment">
+        <div className="comment">
+          <p className="p1">
+            Я надолго запомню мой День рождения, проведённый в этом ресторане!
+            Кусочек родной Армении!!! Отдельное спасибо за комплепент в виде
+            фруктовой тарелки. Будем рекомендовать этот ресторан своим друзьям и
+            родственникам также за рубежом, путешествующих в Санкт-Петербург!!!
+          </p>
+          <img src={man} alt="" />
+          <p className="p2">Посетитель</p>
+          <p className="p3">Николай</p>
+          <div className="btngr">
+            <button className="btn"></button>
+            <button className="btn"></button>
+            <button className="btn"></button>
           </div>
         </div>
       </div>
