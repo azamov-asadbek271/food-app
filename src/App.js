5d931e18e97998507e9bd625bdb6d21257f551fb
azamov-asadbek271 <azamjonovasa45@gmail.com>
2023-06-13 07:14:05 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
+import Header from './Components/Header/Header';
 import Main from './Components/Main/Main';
-import Header from './header';
+
 
 function App() {
   return (
     <div className="App">
-     <Header/>
-   
+    <Header/>
 
      {/* Behruzbek */}
      <Main/>
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,36 @@
-import React from 'react'
+
+import React from "react";
+import "./Header.css"
+// import  from "../IMG/LOGO.png";
+import {FiShoppingCart,FiPhoneCall} from "react-icons/fi"
 
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <header>
+      <div className="navbar">
+        <div className="navbar-logo">
+          <img src="" alt="bu yerda logo joylashgan" />
+        </div>
+        <div className="navbar-link">
+          <a href="#">ГЛАВНАЯ</a>
+          <a href="#">МЕНЮ</a>
+          <a href="#">О НАС</a>
+          <a href="#">БРОНЬ</a>
+          <FiShoppingCart />
+        </div>
+        <div className="navbar-order">
+          <div className="phone">
+            <div className="navbar-phone-number">
+              <FiPhoneCall/>
+              <a href="">+999-888-76-54</a>
+            </div>
+            <p className="navbar-order-paragrph">Свяжитесь с нами для бронирования</p>
+          </div>
+          <button className="btn">ЗАКАЗ СТОЛИКА</button>
+        </div>
+      </div>
+    </header>
+  );
+  
 }
-
-export default Header
+export default Header;
